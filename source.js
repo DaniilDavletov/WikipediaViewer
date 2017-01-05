8106023479b797e58608e652a8a61ecabfb2739c
Daniil <d.daniel.d@list.ru>
2017-01-05 21:10:10 +0300

    Added on submit function

--- source.js
+++ source.js
@@ -2,15 +2,15 @@ var url;
 var myQuerry;
 
 $(document).ready(function(){
-	$("#randBtn").on("click", function(){
+	$("form").on("submit", function(){
 		var result = "";
-		myQuerry = "Pierre+Bourdin";
+		myQuerry = document.getElementById("querry").value;
 		url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=";
 		url += myQuerry;
 		$.getJSON(url, function(json){
 			for (var j = 0; j < json[1].length; j++) {
 				result += "<div class='block'>";
-				result += "<a href='" + json[3][j] + "'>";
+				result += "<a target='_blank' href='" + json[3][j] + "'>";
 				result += json[1][j] + ", " + json[2][j];
 				result += "</a>";
 				result += "<br>";
@@ -18,5 +18,6 @@ $(document).ready(function(){
 				$(".myText").html(result);
 			}
 		});
+		return false;
 	});
 });
