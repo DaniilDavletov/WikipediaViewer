d2306f7e23a243ab0addb2c0ec076abc8ce61e21
Daniil <d.daniel.d@list.ru>
2017-01-05 19:19:34 +0300

    getJson function is now working for static serach querry

--- source.js
+++ source.js
@@ -1,17 +1,22 @@
-var url="http://ipinfo.io/json"
-
-/*$(document).ready(function(){
-	$("#getMessage").on("click", function(){
-		$.getJSON(url, function(json){
-			$(".text").html(JSON.stringify(json));
-		});
-	});
-});*/
+var url;
+var myQuerry;
 
 $(document).ready(function(){
-	//$("#getMessage").on("click", function(){
+	$("#randBtn").on("click", function(){
+		var result = "";
+		myQuerry = "Pierre+Bourdin";
+		url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=";
+		url += myQuerry;
 		$.getJSON(url, function(json){
-			$(".myText").html(JSON.stringify(json));
+			for (var j = 0; j < json[1].length; j++) {
+				result += "<div class='block'>";
+				result += "<a href='" + json[3][j] + "'>";
+				result += json[1][j] + ", " + json[2][j];
+				result += "</a>";
+				result += "<br>";
+				result += "</div>";
+				$(".myText").html(result);
+			}
 		});
-	//});
+	});
 });
